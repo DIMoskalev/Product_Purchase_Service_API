421fa9a9bc43920aa77ff78684085efdc785ae7b
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-10-04 00:27:06 +0300

    update alembic migrations (del cartItem class)

--- app/migration/env.py
+++ app/migration/env.py
@@ -8,7 +8,7 @@ from alembic import context
 import sys
 from os.path import dirname, abspath
 from app.database import DATABASE_URL, Base
-from app.models import User, Product, Cart, CartItem
+from app.models import User, Product, Cart
 
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
--- app/models.py
+++ app/models.py
@@ -10,7 +10,10 @@ class User(Base):
     email: Mapped[str_uniq]
     phone: Mapped[str_uniq]
     hashed_password: Mapped[str_null_false]
+    is_active: Mapped[bool_default_true]
     is_admin: Mapped[bool_default_false]
+
+    carts: Mapped["Cart"] = relationship("Cart", back_populates="users")
 
     def __str__(self):
         return (f"{self.__class__.__name__}(id={self.id}, "
@@ -27,6 +30,8 @@ class Product(Base):
     price: Mapped[int_null_false]
     is_active: Mapped[bool_default_true]
 
+    carts: Mapped["Cart"] = relationship("Cart", back_populates="products")
+
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, name={self.name!r}, price={self.price})"
 
@@ -37,26 +42,15 @@ class Product(Base):
 # Создаем модель таблицы корзины
 class Cart(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    user: Mapped["User"] = relationship("User", back_populates="users")
-
-    def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}, user_id={self.user_id})"
-
-    def __repr__(self):
-        return str(self)
-
-
-# Создаем модель таблицы элемента корзины
-class CartItem(Base):
-    cart_id: Mapped[int] = mapped_column(ForeignKey("carts.id"), nullable=False)
     product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     quantity: Mapped[int_null_false] = mapped_column(default=1)
-    cart: Mapped["Cart"] = relationship("Cart", back_populates="items")
-    product: Mapped["Product"] = relationship("Product")
+
+    user: Mapped["User"] = relationship("User", back_populates="carts")
+    product: Mapped["Product"] = relationship("Product", back_populates="carts")
 
     def __str__(self):
-        return (f"{self.__class__.__name__}(id={self.id}, cart_id={self.cart_id}, "
-                f"product_id={self.product_id}, quantity={self.quantity})")
+        return (f"{self.__class__.__name__}(id={self.id}, "
+                f"user_id={self.user_id}, product_id={self.product_id}, quantity={self.quantity})")
 
     def __repr__(self):
         return str(self)
