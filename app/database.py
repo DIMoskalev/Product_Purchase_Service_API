5b01fc2cfa41d1c8e69c7870042a0de112095701
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-10-15 20:23:13 +0300

    add docker and docker compose files, fix bugs in main.py and database.py

--- app/database.py
+++ app/database.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 from app.config import get_db_url, get_test_db_url
 
 # URL для подключения к базе данных PostgreSQL
-DATABASE_URL = get_test_db_url()
-# DATABASE_URL = get_db_url()
+# DATABASE_URL = get_test_db_url()
+DATABASE_URL = get_db_url()
 
 # Создание асинхронного движка
 engine = create_async_engine(DATABASE_URL, echo=True)
--- app/main.py
+++ app/main.py
@@ -1,29 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-from app.database import engine, Base
 from app.users.router import router as router_users
 from app.products.router import router as router_products
 from app.carts.router import router as router_carts
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    # Clean up the ML models and release the resources
-    await engine.dispose()
-
-
-app = FastAPI(lifespan=lifespan)
-
+app = FastAPI()
 
 app.include_router(router_users)
 app.include_router(router_products)
 app.include_router(router_carts)
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
